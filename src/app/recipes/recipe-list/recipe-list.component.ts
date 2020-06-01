19dc55f25ece09819a8e7e113457c7dfba072743
Sabbir-Hossain-SE <sabbir.hossain133045@gmail.com>
2020-06-01 21:52:30 +0600

    First implementation

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Kabab',
+      'Best Kabab ever!',
+      'https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+    ),
+    new Recipe(
+      'Kabab',
+      'Best Kabab ever!',
+      'https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
+    ),
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
